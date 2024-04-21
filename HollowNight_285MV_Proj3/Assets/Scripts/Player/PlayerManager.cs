6ee0819b7456557b1510e01f651300640643bcca
Rachel Huggins <rhuggins7441@gmail.com>
2024-04-21 19:31:08 -0400

    push

---
+++ HollowNight_285MV_Proj3/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private PlayerControls controlScheme;
+    private int maxHealth = 100;
+    private int health;
+    private bool invincible = false;
+    private int weaponLevel = 1;
+
+    private void Awake()
+    {
+        controlScheme.Gameplay.ToggleInvincibility.performed += ctx => ToggleInvincibility();
+    }
+
+    private void ToggleInvincibility()
+    {
+        invincible = !invincible;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        if (health >= damageAmount)
+        {
+            health = Mathf.Clamp(health - damageAmount, 0, maxHealth);
+            if (health == 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    public void Heal(int amountToHeal)
+    {
+        health = Mathf.Clamp(health + amountToHeal, 0, maxHealth);
+    }
+
+    private void LevelUpWeapon()
+    {
+        weaponLevel += 1;
+    }
+
+    private void Die()
+    {
+        // call loss panel or lose life
+    }
+}
